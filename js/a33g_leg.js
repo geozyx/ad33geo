bdcc7becc7bfa1c1ab177ed730eb87ae5b941338
geozyx <133601565+geozyx@users.noreply.github.com>
2024-10-15 14:43:53 -0300

    Update a33g_leg.js

--- js/a33g_leg.js
+++ js/a33g_leg.js
@@ -1,1 +1,13 @@
-function a(){}
+// Função para mapear os atributos (strings) para cores eixo_fd_fne_br101
+const getColorFromAttribute = (attributeValue) => {
+  switch (attributeValue) {
+    case 'FAIXA DOMINIO':
+      return [255, 0, 0]; // vermelho para "alto"
+    case 'FAIXA NAO EDIFICANTE':
+      return [255, 165, 0]; // laranja para "médio"
+    case 'EIXO DA RODOVIA':
+      return [0, 128, 0]; // verde para "baixo"
+    default:
+      return [128, 128, 128]; // cinza para valores desconhecidos
+  }
+};
