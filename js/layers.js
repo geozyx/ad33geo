ba7488da987c902141e2d508d8782aa4c1051358
geozyx <133601565+geozyx@users.noreply.github.com>
2024-10-17 15:37:56 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -46,3 +46,28 @@ function ad33geoLayerLine(idLine, urlJSON, lineWidth, infoKey, textInfoKey, idIn
         }
     });
 }
+
+function ad33geoLayerPolygon(idPoly, urlJSON, fillColor, infoKey, textInfoKey, idInfoModal){
+    return new deck.GeoJsonLayer({
+	id: idPoly,
+	data: urlJSON,
+	filled: true, // Preenche o polígono
+	stroked: true, // Desenha as bordas do polígono
+	getFillColor: fillColor,
+	getLineColor: fillColor, // Cor da linha
+	lineWidthMinPixels: 1, // Largura mínima da linha
+	pickable: true, // Permite que o polígono seja clicado
+	autoHighlight: true,
+	getCursor: 'grab',
+	  
+    onClick: info => {
+            if (info.object) {
+                const propertyValue = info.object.properties[infoKey] || 'N/A';
+                document.getElementById('modalContent').innerHTML = 
+                    `${textInfoKey} / ${propertyValue}`;
+                document.getElementById(idInfoModal).style.display = 'block';
+            }
+        }
+	});
+
+}
