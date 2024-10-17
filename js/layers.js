535103e8efdf5c97a7acb9b0b7c9efbd1d78282b
geozyx <133601565+geozyx@users.noreply.github.com>
2024-10-17 15:23:33 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,61 +1,48 @@
 function ad33geoLayerPoint(idPoint, urlJSON, radiusPoint, fillColor, infoKey, textInfoKey, idInfoModal) {
-    const lGeoJSONLayer = new deck.GeoJsonLayer({
+    return new deck.GeoJsonLayer({
         id: idPoint,
         data: urlJSON,
-        // Styles
+        // Estilos
         filled: true,
         pointRadiusMinPixels: 3,
         pointRadiusScale: 2000,
         getPointRadius: radiusPoint,
         getFillColor: fillColor,
-        // Interactive props
+        // Propriedades interativas
         pickable: true,
         autoHighlight: true,
 
         onClick: info => {
             if (info.object) {
-                // Use infoKey to dynamically access the property
                 const propertyValue = info.object.properties[infoKey] || 'N/A';
-                
-                // Set content for the modal
-                document.getElementById('modalContent').innerHTML =
+                document.getElementById('modalContent').innerHTML = 
                     `${textInfoKey} / ${propertyValue}`;
-
-                // Display the modal
                 document.getElementById(idInfoModal).style.display = 'block';
             }
         }
     });
-	return lGeoJSONLayer;
 }
 
-function ad33geoLayerLine (idLine, urlJSON, lineWidth,infoKey, textInfoKey, idInfoModal) {
-	const lGeoJSONLayer = new deck.GeoJsonLayer({
-	id: idLine,
-	data: urlJSON,
-	filled: true, // Preenche o polígono
-	stroked: true, // Desenha as bordas do polígono
-	getFillColor: (d) => getColorFromAttribute(d.properties[infoKey]),
-	getLineColor: (d) => getColorFromAttribute(d.properties[infoKey]), // Cor da linha
-	lineWidthMinPixels: lineWidth, // Largura mínima da linha
-	pickable: true, // Permite que o polígono seja clicado
-	autoHighlight: true,
-	getCursor: 'grab',
-	  
-    onClick: info => {
-            if (info.object) {
-                // Use infoKey to dynamically access the property
-                const propertyValue = info.object.properties[infoKey] || 'N/A';
-                
-                // Set content for the modal
-                document.getElementById('modalContent').innerHTML =
-                    `${textInfoKey} / ${propertyValue}`;
+function ad33geoLayerLine(idLine, urlJSON, lineWidth, infoKey, textInfoKey, idInfoModal) {
+    return new deck.GeoJsonLayer({
+        id: idLine,
+        data: urlJSON,
+        filled: true,
+        stroked: true,
+        getFillColor: d => getColorFromAttribute(d.properties[infoKey]),
+        getLineColor: d => getColorFromAttribute(d.properties[infoKey]),
+        lineWidthMinPixels: lineWidth,
+        pickable: true,
+        autoHighlight: true,
+        getCursor: () => 'grab',
 
-                // Display the modal
+        onClick: info => {
+            if (info.object) {
+                const propertyValue = info.object.properties[infoKey] || 'N/A';
+                document.getElementById('modalContent').innerHTML = 
+                    `${textInfoKey} / ${propertyValue}`;
                 document.getElementById(idInfoModal).style.display = 'block';
             }
         }
-	});
-	return lGeoJSONLayer;
+    });
 }
-
